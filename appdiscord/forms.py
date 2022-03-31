c5f9fd32f64b2be175cfa393b5efc7d88e227571
Edgar <coperedgar@gmail.com>
2022-03-31 13:38:42 -0600

    Doceavo intento de Deploy

--- appdiscord/forms.py
+++ appdiscord/forms.py
@@ -6,7 +6,7 @@ class ComentariosForm(forms.ModelForm):
       model = Comentarios
       fields = ("nombre", "email", "website", "comentario")
       #fields = '__all__' 
-   nombre = forms.CharInput(widget=forms.TextInput(attrs={'class': 'form-input text-center input-black'}))
+   nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input text-center input-black'}))
    email = forms.EmailInput(widget=forms.EmailInput(attrs={'class': 'form-input'}))
    website = forms.URLInput(widget=forms.URLInput(attrs={'class': 'form-input'}))
    comentario = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-input'}))
