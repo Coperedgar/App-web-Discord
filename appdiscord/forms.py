3129ff716b97d42ce055c3e2d09846711975f17d
Edgar <coperedgar@gmail.com>
2022-04-06 08:54:52 -0500

    19 intento de Deploy

--- appdiscord/forms.py
+++ appdiscord/forms.py
@@ -23,5 +23,5 @@ class loginForm(AuthenticationForm):
       model = User
       fields = ['username', 'password', 'remember_me']
    username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Username', 'class': 'form-control'}))
-   password = forms.CharField(max_length=50, required=True, widget=forms.Password(attrs={'placeholder': 'Password', 'class': 'form-control', 'data-toggle': 'password', 'id': 'password', 'name'='password'}))
+   password = forms.CharField(max_length=50, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control', 'data-toggle': 'password', 'id': 'password', 'name'='password'}))
    remember_me = forms.BooleanField(required=False)
